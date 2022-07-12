f38b4fb1410ee1c09c05c5e06251db5928b85e22
AlexandraZbant <alexandrazbant@gmail.com>
2022-07-12 14:51:31 +0300

    adding query method examples

--- src/main/java/com/alexandrazbant/recipeapp/controllers/IndexController.java
+++ src/main/java/com/alexandrazbant/recipeapp/controllers/IndexController.java
@@ -1,13 +1,34 @@
 package com.alexandrazbant.recipeapp.controllers;
 
+import com.alexandrazbant.recipeapp.domain.Category;
+import com.alexandrazbant.recipeapp.domain.UnitOfMeasure;
+import com.alexandrazbant.recipeapp.repositories.CategoryRepository;
+import com.alexandrazbant.recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    CategoryRepository categoryRepository;
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("Mexican");
+        Optional<UnitOfMeasure> unitOfMeasureOptional=unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is: "+ categoryOptional.get().getId());
+        System.out.println("UOM Id is: "+ unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }

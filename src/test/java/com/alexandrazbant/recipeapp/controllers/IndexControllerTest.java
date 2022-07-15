3ab5eac542e934d67981efcdb87a2a14009bc884
AlexandraZbant <alexandrazbant@gmail.com>
2022-07-15 12:42:20 +0300

    adding mock mvc test example

--- src/test/java/com/alexandrazbant/recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/alexandrazbant/recipeapp/controllers/IndexControllerTest.java
@@ -7,13 +7,18 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 class IndexControllerTest {
 
@@ -29,6 +34,17 @@ class IndexControllerTest {
         MockitoAnnotations.openMocks(this);
         indexController = new IndexController(recipeService);
     }
+
+    @Test
+    public void testMockMVC() throws Exception {
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
 
     @Test
     void getIndexPage() {

f084a0c28f1e85a7cea05e80ed3c5bbff4ec1b96
AlexandraZbant <alexandrazbant@gmail.com>
2022-07-19 18:40:17 +0300

    completing assignement

--- src/main/java/com/alexandrazbant/recipeapp/boostrap/RecipeBootstrap.java
+++ src/main/java/com/alexandrazbant/recipeapp/boostrap/RecipeBootstrap.java
@@ -146,6 +146,9 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
         perfectGuacamole.getCategories().add(americanCategory);
         perfectGuacamole.getCategories().add(mexicanCategory);
+        perfectGuacamole.setUrl("http://www.simplyrecipes.com/recipes/perfect_guacamole/");
+        perfectGuacamole.setServings(4);
+        perfectGuacamole.setSource("Simply Recipes");
 
         //add to return list
         recipes.add(perfectGuacamole);
@@ -206,6 +209,10 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
         tacosRecipe.getCategories().add(americanCategory);
         tacosRecipe.getCategories().add(mexicanCategory);
+
+        tacosRecipe.setUrl("http://www.simplyrecipes.com/recipes/spicy_grilled_chicken_tacos/");
+        tacosRecipe.setServings(4);
+        tacosRecipe.setSource("Simply Recipes");
 
         recipes.add(tacosRecipe);
         return recipes;

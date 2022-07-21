71559220cfd359c7d4ebc98942500e0d047e8d51
AlexandraZbant <alexandrazbant@gmail.com>
2022-07-21 09:45:52 +0300

    adding create new recipe form and controller methods

--- src/main/java/com/alexandrazbant/recipeapp/controllers/RecipeController.java
+++ src/main/java/com/alexandrazbant/recipeapp/controllers/RecipeController.java
@@ -1,10 +1,10 @@
 package com.alexandrazbant.recipeapp.controllers;
 
+import com.alexandrazbant.recipeapp.commands.RecipeCommand;
 import com.alexandrazbant.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
@@ -22,5 +22,19 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(id));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
